6b514a9fff48841c5e0ccfa656c051513bd24ec1
Ryan Baermann <rcbaermann@gmail.com>
2019-10-02 15:36:59 -0700

    initial commit

---
+++ src/Gorilla.java
@@ -0,0 +1,17 @@
+
+public class Gorilla extends Mammal{
+	public Gorilla() {
+	}
+	
+	public void throwSomething() {
+		energy -= 5;
+	}
+	
+	public void eatBanana() {
+		energy += 10;
+	}
+	
+	public void climb() {
+		energy -= 10;
+	}
+}
---
+++ src/Mammal.java
@@ -0,0 +1,12 @@
+
+public class Mammal {
+	public int energy;
+	public Mammal() {
+		energy = 100;
+	}
+	
+	public int displayEnergy() {
+		System.out.println(energy);
+		return energy;
+	}
+}
